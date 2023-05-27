015aeef1d4a4acfc218be0bece0867e88699331f
alvinhill <rainfirex@att.net>
2023-05-27 16:38:23 -0700

    ONE PAGE WEBSITE AND CSS FOR STEP 399

---
+++ hellow_world.js
@@ -0,0 +1,8 @@
+var greeting = "Hellow world!";
+console.log(greeting);
+
+var greeting2 = "Welcome to Node.js";
+console.log(greeting2);
+
+var sentence1 = "learning how to debug code with the debugger";
+console.log(sentence1);
